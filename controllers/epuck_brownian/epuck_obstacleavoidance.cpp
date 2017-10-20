27afa384db918208bbab3ef24e91b3e3e12817af
Brendan Donohoe <bddonohoe@unm.edu>
2017-10-20 09:23:27 -0600

    Added code we made during our group reading this morning - documentation forthcoming, should be up later this afternoon.

--- controllers/epuck_brownian/epuck_obstacleavoidance.cpp
+++ controllers/epuck_brownian/epuck_obstacleavoidance.cpp
@@ -1,19 +1,12 @@
 /* Include the controller definition */
-#include "epuck_brownian.h"
+#include "epuck_obstacleavoidance.h"
 /* Function definitions for XML parsing */
 #include <argos3/core/utility/configuration/argos_configuration.h>
-#include <argos3/core/utility/logging/argos_log.h>
-
-#include <cmath>
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <iostream>
 
 /****************************************/
 /****************************************/
 
-CEPuckBrownian::CEPuckBrownian() :
+CEPuckObstacleAvoidance::CEPuckObstacleAvoidance() :
    m_pcWheels(NULL),
    m_pcProximity(NULL),
    m_fWheelVelocity(2.5f) {}
@@ -21,7 +14,7 @@ CEPuckBrownian::CEPuckBrownian() :
 /****************************************/
 /****************************************/
 
-void CEPuckBrownian::Init(TConfigurationNode& t_node) {
+void CEPuckObstacleAvoidance::Init(TConfigurationNode& t_node) {
    /*
     * Get sensor/actuator handles
     *
@@ -39,18 +32,13 @@ void CEPuckBrownian::Init(TConfigurationNode& t_node) {
     *
     * NOTE: ARGoS creates and initializes actuators and sensors
     * internally, on the basis of the lists provided the configuration
-    * file at the <controllers><epuck_brownian><actuators> and
-    * <controllers><epuck_brownian><sensors> sections. If you forgot to
+    * file at the <controllers><epuck_obstacleavoidance><actuators> and
+    * <controllers><epuck_obstacleavoidance><sensors> sections. If you forgot to
     * list a device in the XML and then you request it here, an error
     * occurs.
     */
    m_pcWheels    = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
    m_pcProximity = GetSensor  <CCI_ProximitySensor             >("proximity"    );
-   m_pcPosition  = GetSensor  <CCI_PositioningSensor           >("positioning");
-   m_pcLight     = GetSensor  <CCI_LightSensor                 >("light");
-   m_pcRABA      = GetActuator<CCI_RangeAndBearingActuator     >("range_and_bearing");
-   m_pcRABS      = GetSensor  <CCI_RangeAndBearingSensor       >("range_and_bearing");
-
    /*
     * Parse the configuration file
     *
@@ -59,27 +47,44 @@ void CEPuckBrownian::Init(TConfigurationNode& t_node) {
     * have to recompile if we want to try other settings.
     */
    GetNodeAttributeOrDefault(t_node, "velocity", m_fWheelVelocity, m_fWheelVelocity);
-
-   std::cout << "Init() called" << std::endl;
-
-   goal_x = 4.4;
-   goal_y = 1.3;
-
-   ticks = 0;
-   
-   m_pcRABA->SetData(0, FUNCTIONING);
 }
 
 /****************************************/
 /****************************************/
 
-void CEPuckBrownian::ControlStep()
-{
-  // TODO: Implement ControlStep().
-}
+void CEPuckObstacleAvoidance::ControlStep() {
+   /* Get the highest reading in front of the robot, which corresponds to the closest object */
+   Real fMaxReadVal = m_pcProximity->GetReadings()[0];
+   UInt32 unMaxReadIdx = 0;
+   if(fMaxReadVal < m_pcProximity->GetReadings()[1]) {
+      fMaxReadVal = m_pcProximity->GetReadings()[1];
+      unMaxReadIdx = 1;
+   }
+   if(fMaxReadVal < m_pcProximity->GetReadings()[7]) {
+      fMaxReadVal = m_pcProximity->GetReadings()[7];
+      unMaxReadIdx = 7;
+   }
+   if(fMaxReadVal < m_pcProximity->GetReadings()[6]) {
+      fMaxReadVal = m_pcProximity->GetReadings()[6];
+      unMaxReadIdx = 6;
+   }
 
-void CEPuckBrownian::Destroy() {
-  std::cout << "Destroy() called" << std::endl;
+   /* Do we have an obstacle in front? */
+   if(fMaxReadVal > 0.0f) {
+     /* Yes, we do: avoid it */
+     if(unMaxReadIdx == 0 || unMaxReadIdx == 1) {
+       /* The obstacle is on the left, turn right */
+       m_pcWheels->SetLinearVelocity(m_fWheelVelocity, 0.0f);
+     }
+     else {
+       /* The obstacle is on the left, turn right */
+       m_pcWheels->SetLinearVelocity(0.0f, m_fWheelVelocity);
+     }
+   }
+   else {
+     /* No, we don't: go straight */
+      m_pcWheels->SetLinearVelocity(m_fWheelVelocity, m_fWheelVelocity);
+   }
 }
 
 /****************************************/
@@ -95,4 +100,4 @@ void CEPuckBrownian::Destroy() {
  * controller class to instantiate.
  * See also the configuration files for an example of how this is used.
  */
-REGISTER_CONTROLLER(CEPuckBrownian, "epuck_brownian_controller")
+REGISTER_CONTROLLER(CEPuckObstacleAvoidance, "epuck_obstacleavoidance_controller")
--- controllers/epuck_brownian/epuck_obstacleavoidance.h
+++ controllers/epuck_brownian/epuck_obstacleavoidance.h
@@ -7,11 +7,11 @@
  * wheels to move the robot around.
  *
  * This controller is meant to be used with the XML files:
- *    experiments/epuck_brownian.argos
+ *    experiments/epuck_obstacleavoidance.argos
  */
 
-#ifndef EPUCK_BROWNIAN_H
-#define EPUCK_BROWNIAN_H
+#ifndef EPUCK_OBSTACLEAVOIDANCE_H
+#define EPUCK_OBSTACLEAVOIDANCE_H
 
 /*
  * Include some necessary headers.
@@ -22,14 +22,6 @@
 #include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
 /* Definition of proximity sensor */
 #include <argos3/plugins/robots/generic/control_interface/ci_proximity_sensor.h>
-/* Definition of positioning sensor */
-#include <argos3/plugins/robots/generic/control_interface/ci_positioning_sensor.h>
-/* Definition of light sensor */
-#include <argos3/plugins/robots/generic/control_interface/ci_light_sensor.h>
-/* Definition of range-and-bearing actuator */
-#include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_actuator.h>
-/* Definition of range-and-bearing sensor */
-#include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_sensor.h>
 
 /*
  * All the ARGoS stuff in the 'argos' namespace.
@@ -40,20 +32,20 @@ using namespace argos;
 /*
  * A controller is simply an implementation of the CCI_Controller class.
  */
-class CEPuckBrownian : public CCI_Controller {
+class CEPuckObstacleAvoidance : public CCI_Controller {
 
 public:
 
    /* Class constructor. */
-   CEPuckBrownian();
+   CEPuckObstacleAvoidance();
 
    /* Class destructor. */
-   virtual ~CEPuckBrownian() {}
+   virtual ~CEPuckObstacleAvoidance() {}
 
    /*
     * This function initializes the controller.
     * The 't_node' variable points to the <parameters> section in the XML
-    * file in the <controllers><epuck_brownian_controller> section.
+    * file in the <controllers><epuck_obstacleavoidance_controller> section.
     */
    virtual void Init(TConfigurationNode& t_node);
 
@@ -79,7 +71,7 @@ public:
     * so the function could have been omitted. It's here just for
     * completeness.
     */
-   virtual void Destroy();
+   virtual void Destroy() {}
 
 private:
 
@@ -87,34 +79,16 @@ private:
    CCI_DifferentialSteeringActuator* m_pcWheels;
    /* Pointer to the e-puck proximity sensor */
    CCI_ProximitySensor* m_pcProximity;
-   /* Pointer to the positioning sensor */
-   CCI_PositioningSensor* m_pcPosition;
-   /* Pointer to the light sensor */
-   CCI_LightSensor* m_pcLight;
-   /* Pointer to the range-and-bearing actuator */
-   CCI_RangeAndBearingActuator* m_pcRABA;
-   /* Pointer to the range-and-bearing sensor */
-   CCI_RangeAndBearingSensor* m_pcRABS;
 
    /*
     * The following variables are used as parameters for the
     * algorithm. You can set their value in the <parameters> section
     * of the XML configuration file, under the
-    * <controllers><epuck_brownian_controller> section.
+    * <controllers><epuck_obstacleavoidance_controller> section.
     */
    /* Wheel speed. */
    Real m_fWheelVelocity;
 
-   enum EEmitStatus
-   {
-     MALFUNCTIONING = 0,
-     FUNCTIONING
-   } m_eEmitStatus;
-
-   double goal_x;
-   double goal_y;
-
-   int ticks;
 };
 
 #endif
