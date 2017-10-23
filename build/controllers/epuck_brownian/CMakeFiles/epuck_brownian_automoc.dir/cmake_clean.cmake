a0b035b80f95823fcc3c1efd73fdf96ac1069fed
Brendan Donohoe <bddonohoe@unm.edu>
2017-10-23 08:11:28 -0600

    Removed unnecessary files from the directory.
    Removed all files from the directory except for the epuck_brownian controller and the experiment files.  All other files should be maintained in the user's local directory, but should not be added to the main repository.

--- build/controllers/epuck_brownian/CMakeFiles/epuck_brownian_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "epuck_brownian_automoc.cpp"
-  "CMakeFiles/epuck_brownian_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/epuck_brownian_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/controllers/epuck_mapping/CMakeFiles/epuck_mapping_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "epuck_mapping_automoc.cpp"
-  "CMakeFiles/epuck_mapping_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/epuck_mapping_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
