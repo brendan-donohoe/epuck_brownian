86a235420a0c9ad82f29b9ebf15bd9ce0190cc24
Aakash <hssaka7@lobogit.com>
2017-10-26 11:43:44 -0600

    Added randomness to the moterfailure. The robot experiencing motor failure either only turns right or only left depending on the value of random variable which has value either 1 or 0. The direction which the motor failure robot turns is constant for a cycle of experiment.

--- controllers/epuck_brownian/epuck_brownian.h
+++ controllers/epuck_brownian/epuck_brownian.h
@@ -30,14 +30,12 @@
 #include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_actuator.h>
 /* Definition of range-and-bearing sensor */
 #include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_sensor.h>
-#include <iostream>
-#include <fstream>
+
 /*
  * All the ARGoS stuff in the 'argos' namespace.
  * With this statement, you save typing argos:: every time.
  */
 using namespace argos;
-using namespace std;
 
 /*
  * A controller is simply an implementation of the CCI_Controller class.
@@ -127,9 +125,6 @@ private:
   int current_type;
   int random_turn;
   int tick_wander;
-  bool printed_result;
-  int counter;
-
 
   /*
    * The following variables are used as parameters for the
@@ -151,11 +146,9 @@ private:
   /* Time from the start of the simulation until a robot experiences its
      corresponding "failure".  Unused for a functional robot. */
   int ticks_to_failure;
-
-  /* The x-coordinate of the light beacon. */
-  double light_x;
-  /* The y-coordinate of the light beacon. */
-  double light_y;
+   /* This value determines either the left or right motor is not functioning
+     */
+  int randomMotorFailure;
 };
 
 #endif
