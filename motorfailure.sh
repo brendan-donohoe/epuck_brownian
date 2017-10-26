77c5c7a2c118391a8a7887cc34a57df06c6de50c
Germaine Sy <germainesy@unm.edu>
2017-10-25 23:44:28 -0600

    Bash Script Created
    Created a bash script that runs the experiment 100 times and puts the output in a txt file. I also clean the txt file so that it only has the third output for each experiment (the time it takes for third epucks to hit the light).

--- motorfailure.sh
+++ motorfailure.sh
@@ -1,9 +1,13 @@
 #!/bin/bash 
-         COUNTER=0
-         while [  $COUNTER -lt 100 ]; do
-             echo The counter is $COUNTER
-             argos3 -c experiments/epuck_brownian_motorfailure.argos
-             let COUNTER=COUNTER+1 
-
-         done
-
+COUNTER=0
+touch motorfailure_10epucks.txt
+touch temp.txt;
+touch temp2.txt;
+while [  $COUNTER -lt 100 ]; do
+    argos3 -c experiments/epuck_brownian_motorfailure_10epucks > temp.txt
+    sed '/t=/!d' temp.txt > temp2.txt
+    sed 's/t= //' temp2.txt > temp.txt
+    sed -e '1,2d;4,9d' temp.txt >> motorfailure_10epucks.txt
+done
+rm temp.txt
+rm temp2.txt
